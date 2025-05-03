92dcbc6ef832ff1d0bc81f1b4622b1c84ea8c3fb
Sirohi01 <126563915+Sirohi01@users.noreply.github.com>
2025-05-04 03:38:49 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Make sure repo name is correct
 export default defineConfig({
   plugins: [react()],
+  base: '/FitPro-Demo2/',
 })
